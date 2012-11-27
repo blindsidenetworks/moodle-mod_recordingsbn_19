dbfaa4fb65476f7f6fa10c4d5d6026d9c3055110
jfederico <jesus@123it.ca>
2012-11-27 11:02:27 -0500

    v1.0.9a1: Alignment with 1.0.9 version for moodle 2.x Refactoring to take off jquery and dataTables dependency Signed-off-by: jfederico <jesus@123it.ca>

--- view.php
+++ view.php
@@ -12,10 +12,14 @@
 
 require_once(dirname(dirname(dirname(__FILE__))).'/config.php');
 require_once(dirname(__FILE__).'/lib.php');
+require_once($CFG->dirroot.'/mod/bigbluebuttonbn/locallib.php');
 //require_once($CFG->dirroot.'/lib/ddllib.php');
 
 $id = optional_param('id', 0, PARAM_INT); // course_module ID, or
 $a  = optional_param('a', 0, PARAM_INT);  // recordingsbn instance ID
+
+$action  = optional_param('action', 0, PARAM_TEXT);
+$recordingid  = optional_param('recordingid', 0, PARAM_TEXT);
 
 if ($id) {
     if (! $cm = get_coursemodule_from_id('recordingsbn', $id)) {
@@ -52,30 +56,52 @@ $moderator = has_capability('mod/bigbluebuttonbn:moderate', $context);
 
 add_to_log($course->id, "recordingsbn", "view", "view.php?id=$cm->id", "$recordingsbn->id");
 
-/// Print the page header
+///Set strings to show
+$view_head_recording = get_string('view_head_recording', 'recordingsbn');
+$view_head_course = get_string('view_head_course', 'recordingsbn');
+$view_head_activity = get_string('view_head_activity', 'recordingsbn');
+$view_head_description = get_string('view_head_description', 'recordingsbn');
+$view_head_date = get_string('view_head_date', 'recordingsbn');
+$view_head_length = get_string('view_head_length', 'recordingsbn');
+$view_head_duration = get_string('view_head_duration', 'recordingsbn');
+$view_head_actionbar = get_string('view_head_actionbar', 'recordingsbn');
+
+/// Prepare page header
 $strrecordingsbns = get_string('modulenameplural', 'recordingsbn');
 $strrecordingsbn  = get_string('modulename', 'recordingsbn');
 
 $navlinks = array();
 $navlinks[] = array('name' => $strrecordingsbns, 'link' => "index.php?id=$course->id", 'type' => 'activity');
 $navlinks[] = array('name' => format_string($recordingsbn->name), 'link' => '', 'type' => 'activityinstance');
-
 $navigation = build_navigation($navlinks);
 
-echo '<script type="text/javascript" src="'.$CFG->wwwroot.'/mod/recordingsbn/js/libs/jquery/1.7.2/jquery.min.js"></script>'."\n";
-echo '<script type="text/javascript" src="'.$CFG->wwwroot.'/mod/recordingsbn/js/libs/dataTables/1.9.1/jquery.dataTables.min.js"></script>'."\n";
-echo '<script type="text/javascript" src="'.$CFG->wwwroot.'/mod/recordingsbn/js/recordingsbn.js"></script>'."\n";
+///Initialize table headers
+$table->head  = array ($view_head_recording, $view_head_activity, $view_head_description, $view_head_date, $view_head_duration, $view_head_actionbar);
+$table->align = array ('center', 'center', 'center', 'center', 'center', 'left');
 
-
+/// Print page headers
 print_header_simple(format_string($recordingsbn->name), '', $navigation, '', '', true,
               update_module_button($cm->id, $course->id, $strrecordingsbn), navmenu($course, $cm));
 
-/// Print the main part of the page
 
 // Recordings plugin code
-print_box_start();
 if ( isset($CFG->bigbluebuttonbnSecuritySalt) ) {
     
+    // BigBlueButton Setup
+    $url = trim(trim($CFG->bigbluebuttonbnServerURL),'/').'/';
+    $salt = trim($CFG->bigbluebuttonbnSecuritySalt);
+
+    //Execute actions if there is one and it is allowed
+    if( isset($action) && isset($recordingid) && $moderator ){
+        if( $action == 'show' )
+            bigbluebuttonbn_doPublishRecordings($recordingid, 'true', $url, $salt);
+        else if( $action == 'hide')
+            bigbluebuttonbn_doPublishRecordings($recordingid, 'false', $url, $salt);
+        else if( $action == 'delete')
+            print 'DELETED';
+            //bigbluebuttonbn_doDeleteRecordings($recordingid, $url, $salt);
+    }
+
     $meetingID='';
     $results = get_records_sql('SELECT DISTINCT meetingid, courseid, bigbluebuttonbnid FROM '.$CFG->prefix.'bigbluebuttonbn_log WHERE '.$CFG->prefix.'bigbluebuttonbn_log.courseid='.$course->id. ' AND '.$CFG->prefix.'bigbluebuttonbn_log.record = 1 AND '.$CFG->prefix.'bigbluebuttonbn_log.event = \'Create\';' );
     
@@ -88,55 +114,97 @@ if ( isset($CFG->bigbluebuttonbnSecuritySalt) ) {
                 $meetingID .= ','.$result->meetingid.'['.$group->id.']';
             }
         }
-    
+
     } else {                                    // No groups means that it wont check any other sub-activity
         foreach ($results as $result) {
             if (strlen($meetingID) > 0) $meetingID .= ',';
             $meetingID .= $result->meetingid;
         }
-    
+
     }
-    
-    echo '<script type="text/javascript" >var meetingid = "'.$meetingID.'";</script>'."\n";
-    echo '<script type="text/javascript" >var courseid = "'.$course->id.'";</script>'."\n";
-    echo '<script type="text/javascript" >var ismoderator = "'.($moderator?'true':'false').'";</script>'."\n";
-    echo '<script type="text/javascript" >var wwwroot = "'.$CFG->wwwroot.'";</script>'."\n";
-    
-    echo '<script type="text/javascript" >'."\n";
-    echo '    var joining = "false";'."\n";
-    echo '    var bigbluebuttonbn_view = "after";'."\n";
-    echo '    var view_recording_list_recording = "'.get_string('view_recording_list_recording', 'bigbluebuttonbn').'";'."\n";
-    echo '    var view_recording_list_course = "'.get_string('view_recording_list_course', 'bigbluebuttonbn').'";'."\n";
-    echo '    var view_recording_list_activity = "'.get_string('view_recording_list_activity', 'bigbluebuttonbn').'";'."\n";
-    echo '    var view_recording_list_description = "'.get_string('view_recording_list_description', 'bigbluebuttonbn').'";'."\n";
-    echo '    var view_recording_list_date = "'.get_string('view_recording_list_date', 'bigbluebuttonbn').'";'."\n";
-    echo '    var view_recording_list_duration = "'.get_string('view_recording_list_duration', 'bigbluebuttonbn').'";'."\n";
-    echo '    var view_recording_list_actionbar = "'.get_string('view_recording_list_actionbar', 'bigbluebuttonbn').'";'."\n";
-    echo '    var view_recording_list_actionbar_hide = "'.get_string('view_recording_list_actionbar_hide', 'bigbluebuttonbn').'";'."\n";
-    echo '    var view_recording_list_actionbar_show = "'.get_string('view_recording_list_actionbar_show', 'bigbluebuttonbn').'";'."\n";
-    echo '    var view_recording_list_actionbar_delete = "'.get_string('view_recording_list_actionbar_delete', 'bigbluebuttonbn').'";'."\n";
-    echo '</script>'."\n";
-    
-    echo '    <center>'."\n";
-    echo '      <div id="dynamic"></div>'."\n";
-    echo '      <table cellpadding="0" cellspacing="0" border="0" class="display" id="recordingsbn">'."\n";
-    echo '        <thead>'."\n";
-    echo '        </thead>'."\n";
-    echo '        <tbody>'."\n";
-    echo '        </tbody>'."\n";
-    echo '        <tfoot>'."\n";
-    echo '        </tfoot>'."\n";
-    echo '      </table>'."\n";
-    echo '    </center>'."\n";
-    
+    //If there are meetings with recordings load the data to the table
+    if ( $meetingID != '' ){
+        $recordingsbn = bigbluebuttonbn_getRecordingsArray($meetingID, $url, $salt);
+
+        if( isset($recordingsbn) && !isset($recordingsbn['messageKey']) ){
+            foreach ( $recordingsbn as $recording ){
+                if ( $moderator || $recording['published'] == 'true' ) {
+
+                    $length = 0;
+                    $endTime = isset($recording['endTime'])? intval(str_replace('"', '\"', $recording['endTime'])):0;
+                    $endTime = $endTime - ($endTime % 1000);
+                    $startTime = isset($recording['startTime'])? intval(str_replace('"', '\"', $recording['startTime'])):0;
+                    $startTime = $startTime - ($startTime % 1000);
+                    $duration = intval(($endTime - $startTime) / 60000);
+
+                    //$meta_course = isset($recording['meta_context'])?str_replace('"', '\"', $recording['meta_context']):'';
+                    $meta_activity = isset($recording['meta_contextactivity'])?str_replace('"', '\"', $recording['meta_contextactivity']):'';
+                    $meta_description = isset($recording['meta_contextactivitydescription'])?str_replace('"', '\"', $recording['meta_contextactivitydescription']):'';
+
+                    $actionbar = '';
+                    $params['id'] = $cm->id;
+                    $params['recordingid'] = $recording['recordID'];
+                    
+                    if ( $moderator ) {
+                        ///Set action [show|hide]
+                        if ( $recording['published'] == 'true' ){
+                            $params['action'] = 'hide';
+                        } else {
+                            $params['action'] = 'show';
+                        }
+                        $url = $CFG->wwwroot."/mod/recordingsbn/view.php?id=".$params['id']."&action=".$params['action']."&recordingid=".$params['recordingid'];
+                        ///With text
+                        $actionbar = "<a id='actionbar-".$params['action']."-a-".$recording['recordID']."' title='".get_string($params['action'])."' href='".$url."'>".get_string($params['action'])."</a>";
+                        $actionbar .= '&nbsp;';
+                        ///With icon
+
+                        ///Set action delete
+                        $params['action'] = 'delete';
+                        $url = $CFG->wwwroot."/mod/recordingsbn/view.php?id=".$params['id']."&action=".$params['action']."&recordingid=".$params['recordingid'];
+                        ///With text
+                        $actionbar .= "<a id='actionbar-".$params['action']."-a-".$recording['recordID']."' title='".get_string($params['action'])."' onClick='if(confirm(\"".get_string('view_delete_confirmation', 'recordingsbn')."\")) window.location=\"".$url."\"; return false;' href='#'>".get_string($params['action'])."</a>";
+                        ///With icon
+
+                    }
+
+                    $type = '';
+                    foreach ( $recording['playbacks'] as $playback ){
+                        $type .= '<a href="'.$playback['url'].'" title="'.$playback['type'].'" target="_new">'.$playback['type'].'</a>&#32;';
+                    }
+
+                    //Make sure the startTime is timestamp
+                    if( !is_numeric($recording['startTime']) ){
+                        $date = new DateTime($recording['startTime']);
+                        $recording['startTime'] = date_timestamp_get($date);
+                    } else {
+                        $recording['startTime'] = $recording['startTime'] / 1000;
+                    }
+                    //Set corresponding format
+                    //$format = isset(get_string('strftimerecentfull', 'langconfig'));
+                    //if( !isset($format) )
+                    $format = '%a %h %d %H:%M:%S %Z %Y';
+                    //Format the date
+                    $formatedStartDate = userdate($recording['startTime'], $format, usertimezone($USER->timezone) );
+
+                    $table->data[] = array ($type, $meta_activity, $meta_description, str_replace( " ", "&nbsp;", $formatedStartDate), $duration, $actionbar );
+                    
+                }
+            }
+        }
+
+    }
+
+    //Print the table
+    print_box_start();
+    print_table($table);
+    print_box_end();
+
 } else {
-    
-    print_error('view_error_bigbluebuttonbn_not_installed', 'recordingsbn');
-    
+    print_box_start();
+    print_error('view_dependency_error', 'recordingsbn');
+    print_box_end();
+
 }
-print_box_end();
 
 /// Finish the page
 print_footer($course);
-
-?>
